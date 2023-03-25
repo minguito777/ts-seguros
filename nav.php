bb092513c2c8f6a0794ee764c30a8632e0dc354d
Domingo Morelli <mingomorelli67@gmail.com>
2023-03-24 22:21:00 -0300

    testing

---
+++ nav.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.php"><img class="img-logo" src="./img/LOGO.jpeg"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">INICIO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">COTIZÀ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">COMPAÑIAS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">NOSOTROS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SEGUROS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTACTO</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+
+  <div class="phones">
+    <a href="#"><img src="./img/icons8-teléfono-sin-utilizar-50.png" alt="telefono"><p>1555555555</p></a>
+  </div>
+</nav>
---
+++ slide.php
@@ -0,0 +1,30 @@
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/automor.jpg" class="d-block w-100 carrouselImg" alt="vehiculos">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/moto.jpg" class="d-block w-100 carrouselImg" alt="motovehiculo">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/bicicleta.jpg" class="d-block w-100 carrouselImg" alt="bicicleta">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/accidente.jpg" class="d-block w-100 carrouselImg" alt="accidente">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/celular-notebook.jpg" class="d-block w-100 carrouselImg" alt="celular-notebook">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/incendio.jpg" class="d-block w-100 carrouselImg" alt="incendio">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
