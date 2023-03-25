68cde99f7b329d13afe70352676c51747546f74e
Domingo Morelli <mingomorelli67@gmail.com>
2023-03-24 22:50:20 -0300

    viendo

--- slide.php
+++ slide.php
@@ -1,30 +1,54 @@
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./img/automor.jpg" class="d-block w-100 carrouselImg" alt="vehiculos">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="botonCarrousel">
+                <img src="./img/automor.jpg" class="d-block w-100 carrouselImg" alt="vehiculos">
+                <p>Conocé nuestros beneficios pensados para tu comodidad.</p>
+                <button type="button" class="btn btn-secondary">SOLICITAR COTIZACIÒN</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="botonCarrousel">
+                <img src="./img/moto.jpg" class="d-block w-100 carrouselImg" alt="motovehiculo">
+                <p>Solicitá tu cotización aquí.</p>
+                <button type="button" class="btn btn-secondary">SOLICITAR COTIZACIÒN</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="botonCarrousel">
+                <img src="./img/bicicleta.jpg" class="d-block w-100 carrouselImg" alt="bicicleta">
+                <p>Asegurá lo que te hace bien.</p>
+                <button type="button" class="btn btn-secondary">SOLICITAR COTIZACIÒN</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="botonCarrousel">
+                <img src="./img/accidente.jpg" class="d-block w-100 carrouselImg" alt="accidente">
+                <p>Conocé nuestros beneficios pensados para tu comodidad.</p>
+                <button type="button" class="btn btn-secondary">SOLICITAR COTIZACIÒN</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="botonCarrousel">
+                <img src="./img/celular-notebook.jpg" class="d-block w-100 carrouselImg" alt="celular-notebook">
+                <p>Solicitá tu cotización aquí.</p>
+                <button type="button" class="btn btn-secondary">SOLICITAR COTIZACIÒN</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="botonCarrousel">
+                <img src="./img/incendio.jpg" class="d-block w-100 carrouselImg" alt="incendio">
+                <p>Existe una cobertura para proteger tu información.</p>
+                <button type="button" class="btn btn-secondary">SOLICITAR COTIZACIÒN</button>
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
-      <img src="./img/moto.jpg" class="d-block w-100 carrouselImg" alt="motovehiculo">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/bicicleta.jpg" class="d-block w-100 carrouselImg" alt="bicicleta">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/accidente.jpg" class="d-block w-100 carrouselImg" alt="accidente">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/celular-notebook.jpg" class="d-block w-100 carrouselImg" alt="celular-notebook">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/incendio.jpg" class="d-block w-100 carrouselImg" alt="incendio">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 </div>
